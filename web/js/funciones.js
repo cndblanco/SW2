cc67cffb11e10f309d706e979c0f7b8f1618ffda
Lenovo <Lenovo@user>
2017-01-28 01:49:14 -0500

    correccion de login y registro

--- web/js/funciones.js
+++ web/js/funciones.js
@@ -8,26 +8,55 @@
 function login() {
     var user = $("#user").val();
     var psw = $("#psw").val();
-    $.post("Login",{user:user,psw:psw}, function(result){
-        console.log(result);
-        if(result!=="error"){
-            $("#rpta").append("<p style='color: green'>Login correcto</p>");
-            
-        }else{
-            $("#rpta").append("<p style='color: red'>Login incorrecto</p>");
+//    $.get("Login",{user:user,psw:psw}, function(result){
+//        console.log(result);
+//        if(result!=="error"){
+//            $("#rpta").append("<p style='color: green'>Login correcto</p>");
+//            
+//        }else{
+//            $("#rpta").append("<p style='color: red'>Login incorrecto</p>");
+//        }
+//    });
+//    $.post("Login",
+//        {user:user,psw:psw},
+//        function(data,status){
+//            alert("Data: " + data + "\nStatus: " + status);
+//        });
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open("POST", "Login?user=" + user + "&psw=" + psw, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.responseText !== "error") {
+            console.log(this.responseText);
+            document.getElementById("rpta").innerHTML = "<p style='color: green'>Login correcto</p>";
+        } else if (this.responseText === "error") {
+            alert("Usuario no registrado o datos incorrectos");
         }
-    });
+//        } else {
+//            alert("Usuario no registrado o datos incorrectos");
+//        }
+    };
 }
 
 function registrar() {
     var user = $("#user").val();
     var psw = $("#psw").val();
     var nombre = $("#name").val();
-//    $.ajax( { url: "https://api.mlab.com/api/1/databases/sw2/collections/usuarios?apiKey=qDp3t6eIDTXj_etsy2MGShdKI_dQLKgw",
-//		  data: JSON.stringify(  { user: user,psw : psw, nombre : nombre} ),
-//		  type: "POST",
-//		  contentType: "application/json" } );
-    $.post("Registrar",{user:user,psw:psw,nombre:nombre}, function(result){
-        alert("¡Registro correcto!");
-    });
+
+//    $.post("Registrar", {user: user, psw: psw, nombre: nombre}, function (result) {
+//        alert("¡Registro correcto!");
+//    });
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open("POST", "Registrar?user=" + user + "&psw=" + psw + "&nombre=" + nombre, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("rpta").innerHTML = "<p style='color: green'>Registro correcto</p>";
+            $("#user").val("");
+            $("#psw").val("");
+            $("#name").val("");
+        }
+    };
 }
