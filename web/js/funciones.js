e15f01a5db3c2abfd5667bd86c87fc9863646e6f
Lenovo <Lenovo@user>
2017-02-10 01:36:36 -0500

    login & registros preloaders

--- web/js/funciones.js
+++ web/js/funciones.js
@@ -4,8 +4,14 @@
  * and open the template in the editor.
  */
 
-function showStudent(){
-    document.getElementsByName("action").disabled = true;
+function ocultarPreloader() {
+    $("#log_std").hide();
+    $("#reg_std").hide();
+    $("#log_t").hide();
+    $("#reg_teach").hide();
+}
+
+function showStudent() {
     $("#icon_teacher").css('background-color', 'transparent');
     $("#icon_student").css('background-color', '#ff8c00');
     $("#teacher").hide();
@@ -13,22 +19,22 @@ function showStudent(){
     $("#student").show(1000);
 }
 
-function showTeacher(){
+function showTeacher() {
     $("#icon_student").css('background-color', 'transparent');
     $("#icon_teacher").css('background-color', '#ff8c00');
     $("#student").hide();
     $("#reg_teacher").hide();
-    $("#teacher").show(1000);    
+    $("#teacher").show(1000);
 }
 
-function showRegStudent(){
+function showRegStudent() {
     $("#icon_teacher").css('background-color', 'transparent');
     $("#icon_student").css('background-color', '#ff8c00');
     $("#student").hide();
     $("#reg_student").show(1000);
 }
 
-function showRegTeacher(){
+function showRegTeacher() {
     $("#icon_student").css('background-color', 'transparent');
     $("#icon_teacher").css('background-color', '#ff8c00');
     $("#teacher").hide();
@@ -36,10 +42,13 @@ function showRegTeacher(){
 }
 
 function loginStd() {
+    $("#btn_login_std").hide();
+    $("#btn_a_std").hide();
+    $("#log_std").show();
     var user = $("#user_std").val();
     var psw = $("#psw_std").val();
     var xhttp = new XMLHttpRequest();
-    
+
     xhttp.open("POST", "Login?user=" + user + "&psw=" + psw, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
@@ -53,18 +62,21 @@ function loginStd() {
 }
 
 function loginTeacher() {
+    $("#btn_login_teach").hide();
+    $("#btn_a_teach").hide();
+    $("#log_t").show();
     var user = $("#user_prof").val();
     var psw = $("#psw_prof").val();
 
     var xhttp = new XMLHttpRequest();
-    
+
     xhttp.open("POST", "LoginTeacher?user=" + user + "&psw=" + psw, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.responseText !== "error") {
             console.log(this.responseText);
             //document.getElementById("rpta").innerHTML = "<p style='color: green'>Login correcto</p>";
-            window.location.href='profesor.html';
+            window.location.href = 'profesor.html';
         } else if (this.responseText === "error") {
             alert("Usuario no registrado o datos incorrectos");
         }
@@ -72,12 +84,15 @@ function loginTeacher() {
 }
 
 function registrarStudent() {
+    $("#btn_reg_std").hide();
+    $("#btn_a_reg_std").hide();
+    $("#reg_std").show();
     var user = $("#reg_user_std").val();
     var psw = $("#reg_psw_std").val();
     var nombre = $("#reg_name_std").val();
 
     var xhttp = new XMLHttpRequest();
-    
+
     xhttp.open("POST", "Registrar?user=" + user + "&psw=" + psw + "&nombre=" + nombre, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
@@ -91,12 +106,15 @@ function registrarStudent() {
 }
 
 function registrarTeacher() {
+    $("#btn_reg_teacher").hide();
+    $("#btn_a_reg_teach").hide();
+    $("#reg_teach").show();
     var user = $("#reg_user_teacher").val();
     var psw = $("#reg_psw_teacher").val();
     var nombre = $("#reg_name_teacher").val();
 
     var xhttp = new XMLHttpRequest();
-    
+
     xhttp.open("POST", "RegistrarTeacher?user=" + user + "&psw=" + psw + "&nombre=" + nombre, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
